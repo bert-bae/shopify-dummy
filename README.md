2edba2fbb78ea119c3de2235bf33b68b87fbc932
Elbert Bae <elbert.bae@gmail.com>
2020-02-03 22:26:52 -0800

    Initial setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Shopify Dummy Code
+
+Initial setup
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import {Button} from '@material-ui/core'
+
+const Index = () => {
+  return (
+    <div>
+      <p>Sample app using React and Next.js</p>
+      <Button variant="contained" color="secondary">
+        hello world
+      </Button>
+    </div>
+  )
+}
+
+export default Index
